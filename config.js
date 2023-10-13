634cb457d2be7d09546373da39e12496eb504195
davinchy2410 <147827335+davinchy2410@users.noreply.github.com>
2023-10-13 10:00:12 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // MODIFICA ESTA URL POR LA DEL SERVICIO WEB DESPUES DE HABERLO DESPLEGADO
-const AS_URL_BASE = "https://script.google.com/macros/s/AKfycbyeMYLGUHfxUREQiQcm6rXLTuC__kxer8jHwWIaduNfNP2jj3e9oQb6aisJGbD2YoG6EA/exec";
+const AS_URL_BASE = "https://calendar.google.com/calendar/embed?src=35595ad43f367897ef8728040a9c07382a33d36afb722ccf049b58a18fc686a3%40group.calendar.google.com&ctz=UTC";
